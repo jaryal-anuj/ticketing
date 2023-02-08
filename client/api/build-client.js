9e18d16e1e4f669438730ad990673532d2001ea6
jaryal-anuj <jariyal.anuj@gmail.com>
2023-02-08 09:59:07 +0530

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default ({ req })=>{
-    console.log(req.headers.host);
+    //console.log(req.headers.host);
     if(typeof window === 'undefined'){
         let baseURL = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
         if(req.headers.host.includes('anujdev-apps.online')){
